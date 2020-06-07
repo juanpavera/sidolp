2aee2592b658ca146840549e30978ea4bf3c3aa7
Juan Pablo Vera Hurtado <jvera@sudseguros.com>
2020-06-07 19:49:26 -0400

    JP: master

--- inicio.php
+++ inicio.php
@@ -13,5 +13,11 @@
 		<div class="col-md-12 col-xs-12">
 			<img src="images/slide/OH1E453.jpg" alt="COSO" width="100%">
 		</div>
+        <div class="col-md-12 col-xs-12">
+            <img src="images/slide/OG2O040.jpg" alt="COSO" width="100%">
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <img src="images/slide/OH1E453.jpg" alt="COSO" width="100%">
+        </div>
 	</div>
 </div>
