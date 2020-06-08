1cb6122f878ba5d602ecd6999e2d5d785e9865a6
Juan Pablo Vera Hurtado <juanpaverah@gmail.com>
2020-06-07 20:37:44 -0400

    actualizacion 3

--- inicio.php
+++ inicio.php
@@ -28,4 +28,18 @@
             <img src="images/slide/OH1E453.jpg" alt="img4" width="100%">
         </div>
     </div>
+
+    <div class="slideini col-md-12 col-xs-12">
+        <div class="col-md-12 col-xs-12">
+            <img src="images/slide/O70LL00.jpg" alt="img1" width="100%">
+        </div>
+        <img src="images/slide/OH1E230.jpg" alt="img2" width="100%">
+    </div>
+    <div class="col-md-12 col-xs-12">
+        <img src="images/slide/OG2O040.jpg" alt="img3" width="100%">
+    </div>
+    <div class="col-md-12 col-xs-12">
+        <img src="images/slide/OH1E453.jpg" alt="img4" width="100%">
+    </div>
+    </div>
 </div>
