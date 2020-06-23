734192412b6d264451469098e273aa5d7445e21f
juanpavera <juanpaverah@gmail.com>
2020-06-23 09:36:30 -0400

    JPDs

--- funciones.class.php
+++ funciones.class.php
@@ -257,5 +257,39 @@ class Sidolp extends MySQLi
 	        case '12': return 'Diciembre';
 	    endswitch;
 	}
+
+    function nm($month){
+        switch ($month):
+            case '01': return 'Enero';
+            case '02': return 'Febrero';
+            case '03': return 'Marzo';
+            case '04': return 'Abril';
+            case '05': return 'Mayo';
+            case '06': return 'Junio';
+            case '07': return 'Julio';
+            case '08': return 'Agosto';
+            case '09': return 'Septiembre';
+            case '10': return 'Octubre';
+            case '11': return 'Noviembre';
+            case '12': return 'Diciembre';
+        endswitch;
+    }
+
+    function as($month){
+        switch ($month):
+            case '01': return 'Enero';
+            case '02': return 'Febrero';
+            case '03': return 'Marzo';
+            case '04': return 'Abril';
+            case '05': return 'Mayo';
+            case '06': return 'Junio';
+            case '07': return 'Julio';
+            case '08': return 'Agosto';
+            case '09': return 'Septiembre';
+            case '10': return 'Octubre';
+            case '11': return 'Noviembre';
+            case '12': return 'Diciembre';
+        endswitch;
+    }
 }
 ?>
